799bb3b7e7cfe82fdbb992941b68701d18631541
Michael Jacob Delos Santos <90962435+Michael679089@users.noreply.github.com>
2022-04-25 16:12:40 +0800

    Add files via upload

--- firstPython.py
+++ firstPython.py
@@ -1,17 +1,49 @@
 import random
 
-# Variables
-age = random.randint(1, 100)
-guess = int(input('enter your guess : '))
-print(f'my generated age is = {age}')
-# The int converts the string from input to an int
-# If the input is an invalid integer (input is letter), it will throw an error
+score = 0
 
-if guess > age:
-    print("your guess is higher.")
-elif guess < age:
-    print("your guess is lower.")
-else: 
-    print("your guess is correct")
-    guess = int(input('enter your guess : '))
+# Functions
+def StartGuessingGame():
+    # For the Scoring
+    global score
+    print(f'Your Score: {score}')
 
+    # Variables
+    age = random.randint(1, 100)
+    lives = 3
+
+    print(f"You can guess {lives} times")
+    while lives > 0: 
+        try:
+            guess = int(input("Guess the number : "))   # The int converts the string from input to an int | If the input is an invalid integer (input is letter), it will throw an error
+        except ValueError:                              # Skip the if-else statements and repeat if error
+            print("Invalid Guess")
+            continue                
+
+        if guess > age:
+            print("your guess is higher.")
+            lives -= 1
+        elif guess < age:
+            print("your guess is lower.")
+            lives -= 1
+        else: 
+            print("your guess is correct")
+            lives *= 100
+            score += lives
+            break
+
+        print(f'You have {lives} lives left')
+
+    # Game Over Outputs
+    print(f'\nmy generated age is = {age}')
+    print(f'your last guess = {guess}\n')
+    print(f'Your won: {lives}')
+    print(f'Your Score: {score}')
+    tryAgainInput = int(input("Want to play again? (1 for yes | Any for no) : "))
+    if tryAgainInput == 1:
+        StartGuessingGame()
+    else:
+        return()
+
+
+StartGuessingGame()
